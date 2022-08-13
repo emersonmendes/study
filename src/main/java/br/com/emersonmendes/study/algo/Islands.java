fdc9c1027765a6a2506fb2fcdfbbb716f6cbdb07
Emerson Mendes <emerson.emendes@gmail.com>
2022-08-13 17:36:47 +0100

    melhoria

---
+++ src/main/java/br/com/emersonmendes/study/algo/Islands.java
@@ -0,0 +1,15 @@
+package br.com.emersonmendes.study.algo;
+
+public class Islands {
+
+    // No of rows and columns
+    static final int ROW = 5, COL = 5;
+
+    int countIslands(int M[][]){
+
+        int count = 0;
+
+        return count;
+    }
+
+}

997385f52c5cc219bd223201c60670ed88f28355
Emerson Mendes <emerson.emendes@gmail.com>
2024-04-30 21:39:04 -0300

    new algos

--- src/main/java/br/com/emersonmendes/study/other/Anything.java
+++ src/main/java/br/com/emersonmendes/study/other/Anything.java
@@ -1,25 +1,10 @@
 package br.com.emersonmendes.study.other;
 
+import java.util.Arrays;
+
 public class Anything {
 
     public static void main(String[] args) {
 
-
-        System.out.println(new Anything().solution(10,21,31));
-
-
     }
-
-    public int solution(int[] A) {
-        int current = A[0];
-        int counter = 1;
-
-        while (current != -1) {
-            current = A[current];
-            counter++;
-        }
-
-        return counter;
-    }
-
 }
